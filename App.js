1f847f404c9a802819e654daaede0c6549e31783
pro804 <petsoulis.appdev@gmail.com>
2025-07-02 13:35:52 +0300

    feat: Initialize project with theme system,styled-components -Established JavaScript development enviroment -Implemented global theme architecture:   -Colors palette system   -Typography/font scales   -Responsive size constants   -Spacing system (margin/paddings) -Configured styled-components for CSS-in-JS implementation -Created theme provider for app-wide consistency -Designed folder structure for scalable development -Build foundational Restaurants screen -Implemented RestaurantInfoCard component using:   -Styled-components for UI consistency   -Theme-aware styling   -Reusable design patterns Theme system and styled-components configured to enforce design consistency across all components

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
 
+import {
+    useFonts as useOswald,
+    Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { theme } from './src/infrastructure/theme';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [oswaldLoaded] = useOswald({
+        Oswald_400Regular,
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const [latoLoaded] = useLato({
+        Lato_400Regular,
+    });
+
+    if (!oswaldLoaded || !latoLoaded) {
+        return null;
+    }
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <RestaurantsScreen />
+            </ThemeProvider>
+            <ExpoStatusBar style="auto" />
+        </>
+    );
+}
